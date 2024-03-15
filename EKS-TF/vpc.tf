d865616f2d8d6651b623a00eeb9afba70f379d8c
christyl23 <christyl23@gmail.com>
2024-03-15 17:39:26 -0400

    updates more

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -1,55 +1,55 @@
-data "aws_vpc" "vpc" {
+data "aws_vpc" "dove" {
   filter {
     name   = "tag:Name"
     values = [var.vpc-name]
   }
 }
 
-data "aws_internet_gateway" "igw" {
+data "aws_internet_gateway" "dove-IGW" {
   filter {
     name   = "tag:Name"
     values = [var.igw-name]
   }
 }
 
-data "aws_subnet" "subnet" {
+data "aws_subnet" "dove-pub-1" {
   filter {
     name   = "tag:Name"
     values = [var.subnet-name]
   }
 }
 
-data "aws_security_group" "sg-default" {
+data "aws_security_group" "dove-sg" {
   filter {
     name   = "tag:Name"
     values = [var.security-group-name]
   }
 }
 
-resource "aws_subnet" "public-subnet2" {
-  vpc_id                  = data.aws_vpc.vpc.id
+resource "aws_subnet" "dove-pub-2" {
+  vpc_id                  = data.aws_vpc.dove.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = var.subnet-name2
+    Name = "dove-pub-2"
   }
 }
 
-resource "aws_route_table" "rt2" {
+resource "aws_route_table" "dove-pub-RT" {
   vpc_id = data.aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
+    gateway_id = data.aws_internet_gateway.dove-IGW.id
   }
 
   tags = {
-    Name = var.rt-name2
+    Name = "dove-pub-RT"
   }
 }
 
-resource "aws_route_table_association" "rt-association2" {
-  route_table_id = aws_route_table.rt2.id
-  subnet_id      = aws_subnet.public-subnet2.id
+resource "aws_route_table_association" "dove-pub-1-a" {
+  route_table_id = aws_route_table.dove-pub-RT.id
+  subnet_id      = aws_subnet.dove-pub-1.id
 }
